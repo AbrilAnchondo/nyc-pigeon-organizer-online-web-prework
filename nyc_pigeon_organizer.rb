a8919c3cf33ed34b807972dcf8aedda38a4b3e2e
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-06-02 18:08:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |property, info|
+    info.each do |key, names|
+      names.each do |name|
+        if pigeon_list.has_key?(name) == false
+           pigeon_list[name] = {}
+        end
+        
+        if pigeon_list.has_key?(property) == false
+           pigeon_list[name][property] = []
+        end
+        
+        if pigeon_list[name][property].include?(key) == false 
+           key = key.to_s
+           pigeon_list[name][property].push(key)
+           puts pigeon_list[name][property].length
+        end
+      end
+    end
+  end   
+  #puts pigeon_list
+  pigeon_list
 end
